1f7ec59c5bf54aa2dee6c4271ea0bf88ca7fffe8
Gianluca Truda <dev@gianluca.ai>
2024-09-09 21:23:54 +0100

    agent.ask_for_input()

--- main.py
+++ main.py
@@ -16,6 +16,20 @@ class Agent:
             return f"Human({self.name})"
         return f"AI({self.name})"
 
+    def ask_for_input(self):
+        _input = None
+        if self.is_player:
+            _input = input(f"@{self.name}> ")
+            # TODO error handling
+        else:
+            _input = "I am a robot, beep boop."
+            # TODO implement LLM call
+
+        return _input
+
+    def notify_of_message(self, m):
+        pass
+
 
 if __name__ == '__main__':
     state = {}
@@ -27,18 +41,20 @@ if __name__ == '__main__':
         _name = f"Eve_{random.randint(100, 999)}"
         state['agents'].append(Agent(_name))
     player = Agent(name="Alice", is_player=True)
-    state['agents'].append(player)
-    print(f"{state['agents']=}")
+state['agents'].append(player)
+print(f"DEBUG: {state['agents']=}")
 
-    state['messages'] = []
+state['messages'] = []
 
-    print(conf['messages']['welcome'])
-    while True:
-        try:
-            player_input = input(f"@{player.name}> ")
-            if len(player_input) > 0:
-                state['messages'].append((player.name, player_input))
-            print(state['messages'])
-        except KeyboardInterrupt:
-            print("\nYou quit")
-            sys.exit(1)
+print(conf['messages']['welcome'])
+while True:
+    try:
+        for agent in state['agents']:
+            msg = agent.ask_for_input()
+            # TODO error handling
+            state['messages'].append((agent.name, msg))
+            print(f"@{agent.name}: {msg}")
+            # print(f"DEBUG: {state['messages']=}")
+    except KeyboardInterrupt:
+        print("\nYou quit")
+        sys.exit(1)
